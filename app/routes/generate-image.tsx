a5351de1532b805fe7678e1718dbf9bb70b2fe46
gwuluo <admin@huhonggang.com>
2025-04-19 22:48:47 +0800

    Update generate-image.tsx

--- app/routes/generate-image.tsx
+++ app/routes/generate-image.tsx
@@ -97,7 +97,7 @@ const GenerateImage: FC = () => {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 px-4">
       <div className="relative bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-3xl shadow-2xl p-10 max-w-md w-full">
         <h1 className="text-4xl font-extrabold text-white mb-8 text-center drop-shadow-lg">
-          白嫖 CF 的 Flux 生成图片
+          Flux 生成图片
         </h1>
         <Form method="post" className="space-y-8" onSubmit={handleSubmit}>
           <div>
@@ -146,6 +146,7 @@ const GenerateImage: FC = () => {
             >
               <option value="512x512">512x512</option>
               <option value="768x768">768x768</option>
+             <option value="1024x768">1024x1024</option>
               <option value="1024x1024">1024x1024</option>
             </select>
           </div>
